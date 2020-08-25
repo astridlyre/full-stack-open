0baef31e9918df6a2afd1e46f18d8e889ae7402e
astridlyre <51189118+astridlyre@users.noreply.github.com>
2020-08-24 22:27:19 -0400

    Exercise 1.2 completed

--- part1/src/index.js
+++ part1/src/index.js
@@ -2,24 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const Header = (props) => <h1>{props.course}</h1>;
+const Part = (props) => (
+  <p>
+    {props.part} {props.exercise}
+  </p>
+);
 const Content = (props) => (
-  <>
-    <p>
-      {props.part[1]} {props.exercises[1]}
-    </p>
-    <p>
-      {props.part[2]} {props.exercises[2]}
-    </p>
-    <p>
-      {props.part[3]} {props.exercises[3]}
-    </p>
-  </>
+  <div>
+    <Part part={props.part[1]} exercise={props.exercises[1]} />
+    <Part part={props.part[2]} exercise={props.exercises[2]} />
+    <Part part={props.part[3]} exercise={props.exercises[3]} />
+  </div>
 );
 const Total = (props) => {
   return (
     <p>
       Number of exercises{" "}
-      {props.exercises[1] + props.exercises[2] + props.exercises[3]}
+      {props.exercises[3] + props.exercises[2] + props.exercises[3]}
     </p>
   );
 };
@@ -34,11 +33,11 @@ const App = () => {
   const exercises = { "1": 10, "2": 7, "3": 14 };
 
   return (
-    <div>
+    <>
       <Header course={course} />
       <Content part={part} exercises={exercises} />
       <Total exercises={exercises} />
-    </div>
+    </>
   );
 };
 
