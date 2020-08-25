34bc9b6bf56696e1c39bd4b86819a4eee1babaf1
astridlyre <51189118+astridlyre@users.noreply.github.com>
2020-08-24 22:54:21 -0400

    Exercise 1.5 completed

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const Header = (props) => <h1>{props.course}</h1>;
+const Header = (props) => <h1>{props.name}</h1>;
+
 const Part = (props) => (
   <p>
     {props.part.name} {props.part.exercises}
   </p>
 );
+
 const Content = (props) => (
   <div>
     <Part part={props.parts[0]} />
@@ -14,6 +16,7 @@ const Content = (props) => (
     <Part part={props.parts[2]} />
   </div>
 );
+
 const Total = (props) => {
   return (
     <p>
@@ -26,26 +29,29 @@ const Total = (props) => {
 };
 
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
   return (
     <>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </>
   );
 };
