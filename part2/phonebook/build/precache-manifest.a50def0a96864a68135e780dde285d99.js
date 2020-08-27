591f35fd8c78921f825b71f844765bdf47c72763
unknown <astrid.lyre@outlook.com>
2020-08-26 20:57:09 -0400

    Fixed the path

---
+++ part2/phonebook/build/precache-manifest.a50def0a96864a68135e780dde285d99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4facf81420eb5c01e3cd3d07a52a058",
+    "url": "/full-stack-open/part2/phonebook/build/index.html"
+  },
+  {
+    "revision": "d7a95cab00e00a687921",
+    "url": "/full-stack-open/part2/phonebook/build/static/css/main.b3cac7ef.chunk.css"
+  },
+  {
+    "revision": "1dc5294b31049c576a1f",
+    "url": "/full-stack-open/part2/phonebook/build/static/js/2.47916d12.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/full-stack-open/part2/phonebook/build/static/js/2.47916d12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7a95cab00e00a687921",
+    "url": "/full-stack-open/part2/phonebook/build/static/js/main.df6ee75c.chunk.js"
+  },
+  {
+    "revision": "5bb992267e931c6e0db0",
+    "url": "/full-stack-open/part2/phonebook/build/static/js/runtime-main.2df0e249.js"
+  },
+  {
+    "revision": "c37b01f4c4ccc39ff321ffa6732fe62e",
+    "url": "/full-stack-open/part2/phonebook/build/static/media/bg.c37b01f4.jpg"
+  }
+]);
--- part2/phonebook/build/service-worker.js
+++ part2/phonebook/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/full-stack-open/part2/build/precache-manifest.af20ca4e6d27b08a65e153031a48f61b.js"
+  "/full-stack-open/part2/phonebook/build/precache-manifest.a50def0a96864a68135e780dde285d99.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/full-stack-open/part2/build/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/full-stack-open/part2/phonebook/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
